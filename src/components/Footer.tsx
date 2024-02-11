269fba132003eed8e88d582bf595166495ef5ebb
Mursaleen00 <playessport@gmail.com>
2024-02-12 03:42:31 +0500

    Footer Animation Added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,14 +1,25 @@
+"use client";
 import Image from "next/image";
-import React from "react";
-import Button from "./Button";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 function Footer() {
+  useEffect(() => {
+    AOS.init({
+      duration: 2000, // Animation duration
+      once: true, // Whether animation should happen only once
+    });
+  }, []);
   return (
     <div className="bg-theme-first-color rounded-t-[54px] md:rounded-t-[104px]">
       <div className="bg-theme-bg bg-cover bg-opacity-5 -mt-28 ">
         <div className="bg-theme-first-color-alt bg-opacity-80 rounded-t-[54px] md:rounded-t-[104px] w-full lg:px-[150px] px-4 pb-[172px] pt-10 lg:pt-[140px] relative">
           <div className="flex flex-col lg:flex-row justify-center md:justify-around">
-            <div className="flex flex-col justify-center items-center gap-y-2 ">
+            <div
+              className="flex flex-col justify-center items-center gap-y-2 "
+              data-aos="fade-right"
+            >
               <div className="flex gap-x-2 text-lg items-center justify-center">
                 <Image
                   alt="Logo"
@@ -24,7 +35,7 @@ function Footer() {
                 </h1>
               </div>
             </div>
-            <div className="flex flex-col justify-center items-center gap-y-5">
+            <div className="flex flex-col justify-center items-center gap-y-5" data-aos="fade-left">
               <input
                 type="text"
                 className="bg-transparent border-2 rounded-full border-theme-second-color px-5 py-3 w-full max-w-[400px]"
@@ -39,7 +50,7 @@ function Footer() {
               </p>
             </div>
           </div>
-          <div className="w-full flex gap-x-5 justify-center mt-10">
+          <div className="w-full flex gap-x-5 justify-center mt-10" data-aos="zoom-in-down">
             <Image
               alt="Logo"
               src={"/icon/facebook.svg"}
